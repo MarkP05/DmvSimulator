ddc49f73966ae05a9d07592ba97c2e882aa425a7
MarkP05 <mvpfister5@gmail.com>
2025-03-05 17:06:54 -0500

    Added 1% chance of successful paperwork

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -12,7 +12,12 @@ public class DmvSimulator {
             // When the the generated number is the one in the loop, breaks the loop
             if (dmvNum == currNum) {
                 System.out.println("Hey it is finally your turn!");
-                System.out.println("Bad News... this paperwork is useless...");
+                int luck = (int) (Math.random()*100); // Generates a random number between 0 and 100
+                if (luck == 1) { // This makes chance of successful paperwork 1%
+                    System.out.println("Luckily for you, your paperwork is correct! I have no idea how that is possible...");
+                } else {
+                    System.out.println("Bad News... this paperwork is useless...");
+                }
                 break;
             }
 
